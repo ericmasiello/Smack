d8e9c18a71ca3ffccf93b07b74c1f0d44d70e806
Eric Masiello <eric.j.masiello@gmail.com>
2018-01-20 08:48:58 -0500

    Add VCs and configure hamburger for revealing ChannelVC

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Eric Masiello on 1/20/18.
+//  Copyright © 2018 Eric Masiello. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+}
