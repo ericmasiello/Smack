ef52cfc3e503e2ee43264fc5a355388fdf19dee7
Eric Masiello <eric.j.masiello@gmail.com>
2018-01-20 17:23:44 -0500

    Add login view

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Eric Masiello on 1/20/18.
+//  Copyright © 2018 Eric Masiello. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Eric Masiello on 1/20/18.
+//  Copyright © 2018 Eric Masiello. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
