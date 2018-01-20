309976d6d705a9b753b104f5b26d0a5d35872c8e
Eric Masiello <eric.j.masiello@gmail.com>
2018-01-20 16:36:52 -0500

    Create a GraidientView and apply to channel and chat views

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,37 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Eric Masiello on 1/20/18.
+//  Copyright © 2018 Eric Masiello. All rights reserved.
+//
+
+import UIKit
+
+// makes it work in storyboard
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            // updates the layout
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    // callled whenever setNeedsLayout() is called
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0) // makes it the bottom most layer
+    }
+}
