eb08660c6211b434bb1dc82bee098f7e31eca1af
Eric Masiello <eric.j.masiello@gmail.com>
2018-01-25 13:14:41 -0500

    Wire up registering a user to the database from the UI

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,8 +10,34 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func createAcctPressed(_ sender: Any) {
+        // unwrap optionals, return if they are empty
+        guard let email = emailTxt.text, emailTxt.text != "" else { return }
+        guard let pass = emailTxt.text, passTxt.text != "" else { return }
+     
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user!")
+            } else {
+                print("failed to register user :(")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
     }
     
     @IBAction func closePressed(_ sender: Any) {
