352401db475f8a94298a96c80c69929ce0fbddf7
Eric Masiello <eric.j.masiello@gmail.com>
2018-01-27 22:08:26 -0500

    Refactor JSON parsing using SwiftyJSON

--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
     static let instance = AuthService()
@@ -72,19 +73,18 @@ class AuthService {
         Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
             
             if response.result.error == nil {
-                if let json = response.result.value as? Dictionary<String, Any> {
-                    if let email = json["user"] as? String {
-                        self.userEmail = email
-                    }
-                    
-                    if let token = json["token"] as? String {
-                        self.authToken = token
-                    }
+                guard let data = response.data else { return }
+                do {
+                    let json = try JSON(data: data)
+                    self.userEmail = json["user"].stringValue
+                    self.authToken = json["token"].stringValue
+                    self.isLoggedIn = true
+                    completion(true)
+                } catch {
+                    print("Error parsing JSON")
+                    completion(false)
                 }
                 
-                self.isLoggedIn = true
-                
-                completion(true)
             } else {
                 completion(false)
                 debugPrint(response.result.error as Any)
