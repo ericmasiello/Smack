f5f480f85af535205aad03105ed86c48f3b15b0e
Eric Masiello <eric.j.masiello@gmail.com>
2018-01-29 13:01:48 -0500

    Animate bg color change

---
+++ Smack/View/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Eric Masiello on 1/29/18.
+//  Copyright © 2018 Eric Masiello. All rights reserved.
+//
+
+import UIKit
+
+class CircleImage: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
