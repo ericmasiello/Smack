0a873eff8db3d05cecf96f68be3b9e342e723676
Eric Masiello <eric.j.masiello@gmail.com>
2018-02-09 16:59:51 -0500

    Fetch channels from API

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Eric Masiello on 2/9/18.
+//  Copyright © 2018 Eric Masiello. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
