c3fac93409d4e13a153be6b420823bbd4502fdb4
Eric Masiello <eric.j.masiello@gmail.com>
2018-02-23 08:27:44 -0500

    Remove all channels when the user logs out

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -14,6 +14,7 @@ class MessageService {
     static let instance = MessageService()
     
     var channels = [Channel]()
+    var selectedChannel: Channel?
     
     func findAllChannel(completion: @escaping CompletionHandler) {
         Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
@@ -44,4 +45,7 @@ class MessageService {
         }
     }
     
+    func clearChannels() {
+        channels.removeAll()
+    }
 }
