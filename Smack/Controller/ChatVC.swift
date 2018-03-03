1123d8dfc7c6c0aeadff508222357b9288496ceb
Eric Masiello <eric.j.masiello@gmail.com>
2018-03-03 14:29:37 -0500

    Allow table row to size dynamically based on message

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -22,6 +22,9 @@ class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         
+        tableView.estimatedRowHeight = 80
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
         view.bindToKeyboard()
         let tap = UITapGestureRecognizer(target: self, action: #selector(ChatVC.handleTap))
         view.addGestureRecognizer(tap)
