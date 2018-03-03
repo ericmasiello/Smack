b7978dea2673ba7557dfa2941e90ebd1154ebb5d
Eric Masiello <eric.j.masiello@gmail.com>
2018-03-03 14:04:10 -0500

    Send message from ChatVC to backend

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -78,6 +78,19 @@ class ChatVC: UIViewController {
     }
     
     @IBAction func sendMessagePressed(_ sender: Any) {
+        if AuthService.instance.isLoggedIn {
+            guard let channelId = MessageService.instance.selectedChannel?.id else { return }
+            guard let message = messageTxtBox.text else { return }
+            
+            SocketService.instance.addMessage(messageBody: message, userId: UserDataService.instance.id, channelId: channelId, completion: { (success) in
+                if (success) {
+                    self.messageTxtBox.text = ""
+                    self.messageTxtBox.resignFirstResponder()
+                } else {
+                    debugPrint("Error sending messaage")
+                }
+            })
+        }
         
     }
     
