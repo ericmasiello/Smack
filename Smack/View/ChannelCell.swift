e10b21e2d7a8e362c6598356caad1b6bf645b207
Eric Masiello <eric.j.masiello@gmail.com>
2018-03-03 14:25:54 -0500

    Clean up comments and white space

--- Smack/View/ChannelCell.swift
+++ Smack/View/ChannelCell.swift
@@ -11,11 +11,10 @@ import UIKit
 class ChannelCell: UITableViewCell {
     
     // Outlets
-
     @IBOutlet weak var channelName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
